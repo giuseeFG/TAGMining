b1291883d1d9ce75af8347364030c65e9b2bb9a3
AndryGP <andrea.damelio@gmail.com>
2015-06-05 12:51:13 +0200

    crumbs

--- node_parser/server.js
+++ node_parser/server.js
@@ -1,7 +1,7 @@
 var http = require('http'),
-    fs = require('fs'),
-    url = require('url'),
-    parseString = require('xml2js').parseString;
+  fs = require('fs'),
+  url = require('url'),
+  parseString = require('xml2js').parseString;
 
 var WARCStream = require('warc');
 var w = new WARCStream();
@@ -10,51 +10,73 @@ var title = '';
 var trec_id = '';
 var i = 0;
 
+//PHASE 1 -  CREATE DOCS
 fs.createReadStream('/Volumes/MacbookHD/Documenti/MYSTUFF/RM3/2nd/AGIW/TAGMining/new_file.warc')
-    .pipe(w)
-    .on('data', function(data) {
-        if (data.headers["WARC-Type"] === "response") {
-            output.trec_id_long = data.headers["WARC-Trec-ID"];
-            output.content = data.content.toString();
-        //     if (output.content.indexOf('Content-Type: text/html') > -1) {
-        //         try {
-        //             parseString(output.content, {
-        //                 strict: false
-        //             }, function(err, result) {
-        //                 if (
-        //                     (typeof result !== "undefined") &&
-        //                     (typeof result.HTML !== "undefined") &&
-        //                     (typeof result.HTML.HEAD !== "undefined") &&
-        //                     (typeof result.HTML.HEAD[0] !== "undefined") &&
-        //                     (typeof result.HTML.HEAD[0].TITLE !== "undefined")
-        //                 ) {
-        //                     output.title = result.HTML.HEAD[0].TITLE[0];
-        //                 }
-        //             });
-        //         } catch (e) {
-        //             i++;
-        //             console.log("ERRORE NUMERO " + i);
-        //         }
-        //     }
-        }
+  .pipe(w)
+  .on('data', function(data) {
+    if (data.headers["WARC-Type"] === "response") {
+      output.trec_id_long = data.headers["WARC-Trec-ID"];
+      output.content = data.content.toString();
+      if (output.content.indexOf('Content-Type: text/html') > -1) {
         try {
-        var str = output.trec_id_long;
-
-        var doc = {
-            trec_id: output.trec_id_long.substring(output.trec_id_long.length - 5, output.trec_id_long.length),
-            content: output.content
+          parseString(output.content, {
+            strict: false
+          }, function(err, result) {
+            if (
+              (typeof result !== "undefined") &&
+              (typeof result.HTML !== "undefined") &&
+              (typeof result.HTML.HEAD !== "undefined") &&
+              (typeof result.HTML.HEAD[0] !== "undefined") &&
+              (typeof result.HTML.HEAD[0].TITLE !== "undefined")
+            ) {
+              output.title = result.HTML.HEAD[0].TITLE[0];
+            }
+          });
+        } catch (e) {
+          i++;
+          console.log("ERRORE NUMERO " + i);
         }
+      }
+    }
 
-        console.log(doc.trec_id);
-        console.log(" ");
+    try {
+
+      var doc = {
+        trec_id: output.trec_id_long.substring(output.trec_id_long.length - 5, output.trec_id_long.length),
+        title: output.title,
+        content: output.content
+
+      }
+      console.log(doc.trec_id);
+      console.log(doc.title);
+
       
 
 
-        var docs = [];
-        docs.push(doc);
-       }
-       catch(err) {
-        console.log("errore");
-       }
 
-    });
+      var docs = [];
+      docs.push(doc);
+    } catch (err) {
+      console.log("errore");
+    }
+    // extractPhrase(docs);
+  });
+
+// // PHASE 2 - EXTRACT PHRASES
+
+// function extractPhrases(docs) {
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
