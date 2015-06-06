397f1d51538103626493e8de783bb3d8b1cf45c1
AndryGP <andrea.damelio@gmail.com>
2015-06-06 15:55:22 +0200

    split and check done

--- warc_parser.py
+++ warc_parser.py
@@ -1,8 +1,9 @@
 import warc
-f = warc.open("09.warc")
-output = open("new_file.warc", 'w')
+f = warc.open("09small.warc")
+output = open("New09Small.warc", 'w')
 i = 0
 for record in f:
 	output.write(str(record))
-
+	print i
+	i = i+1
 output.close()
